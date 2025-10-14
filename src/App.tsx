1f3b96a0a9f9b44ed1a53035c6621e313aa48ced
plop14 <70171493+plero75@users.noreply.github.com>
2025-10-15 00:28:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,30 @@
-import React from 'react'
-export default function App(){return (<div className='p-6'>App OK</div>)}
+import React from 'react';
+import { Header } from '../components/Header';
+import { TransportSection } from '../components/TransportSection';
+import { VelibCard } from '../components/VelibCard';
+import { SytadinCard } from '../components/SytadinCard';
+import { NewsCard } from '../components/NewsCard';
+import { CoursesSection } from '../components/CoursesSection';
+import { Footer } from '../components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 p-4">
+      <div className="max-w-7xl mx-auto space-y-6">
+        <Header />
+        <TransportSection />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <VelibCard />
+          <NewsCard />
+        </div>
+
+        <SytadinCard />
+        <CoursesSection />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
